402de05e10b9ac66733cbeb4ec39f890693329a6
Bryan MacDiarmid <brymac@gmail.com>
2015-09-18 17:09:57 -0400

    First test, flat markup, passing

--- job.rb
+++ job.rb
@@ -1,23 +1,26 @@
 class Job
 
-def initialize(base_price, people, type)
-  @base_price = base_price
-  @people = people
-  @type = type
-end
+  attr_accessor :base_price, :people, :type
+  def initialize(base_price, people, type)
+    @base_price = base_price.to_f
+    @people = people
+    @type = type
+  end
 
-def flat_markup
+  def flat_markup
+    @base_price * 0.05
+  end
 
-end
+  def people_markup
 
-def people_markup
+  end
 
-end
+  def type_markup
 
-def type_markup
+  end
 
-end
+  def total_price
 
-def total_price
+  end
 
 end
--- spec/job_spec.rb
+++ spec/job_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Job do
+  before :each do
+    @job_1 = Job.new 1299.99, 3, 'food'
+  end
+  describe "#flat_markup" do
+    it 'returns 5% of base price' do
+      expect(@job_1.flat_markup).to eql(@job_1.base_price * 0.05)
+    end
 
+  end
 end
