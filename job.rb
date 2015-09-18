af8d7c555af3051d44713a376bbd87d4584c8f07
Bryan MacDiarmid <brymac@gmail.com>
2015-09-18 17:00:09 -0400

    Initial commit, empty shell for class and tests

---
+++ job.rb
@@ -0,0 +1,23 @@
+class Job
+
+def initialize(base_price, people, type)
+  @base_price = base_price
+  @people = people
+  @type = type
+end
+
+def flat_markup
+
+end
+
+def people_markup
+
+end
+
+def type_markup
+
+end
+
+def total_price
+
+end
